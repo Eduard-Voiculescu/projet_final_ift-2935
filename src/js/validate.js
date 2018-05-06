c4a3f19e1dd19fe6be983ae6fa3b9c033b991335
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2018-05-06 16:58:43 -0400

    fichier

--- src/js/validate.js
+++ src/js/validate.js
@@ -5,6 +5,12 @@ function validateRegisterForm() {
     var password = document.forms["register_form"]["password_register"].value;
     /* Aller chercher le re-type password de l'utilisateur */
     var retype_password = document.forms["register_form"]["retype_password_register"].value;
+    /* Aller chercher le nom de l'utilisateur */
+    var nom = document.forms["register_form"]["nom_register"].value;
+    /* Aller chercher le prénom de l'utilisateur */
+    var prenom = document.forms["register_form"]["prenom_register"].value;
+    /* Aller chercher le numéro de téléphone de l'utilisateur */
+    var num_tel = document.forms["register_form"]["telephone_register"].value;
 
     /* Vérification que l'adresse courriel n'est pas vide */
     if(email_addr == "") {
@@ -29,6 +35,18 @@ function validateRegisterForm() {
     } else if ((password.length < 3 || password.length > 10)
         && (retype_password.length < 4 || retype_password .length > 10)) {
         alert("Passwords has to be more than 3 caracters and less than 11 caracters.");
+        return false;
+    }
+
+    /* Vérifier que nom, prénom et numéro de téléphone n'est pas vide */
+    if(nom == ""){
+        alert("Name field cannot be empty");
+        return false;
+    } else if (prenom == "") {
+        alert("Last name field cannot be empty");
+        return false;
+    } else if (num_tel == "") {
+        alert("Number of telephone field cannot be empty")
         return false;
     }
     return true;
